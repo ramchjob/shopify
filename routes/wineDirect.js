d064e812122cef877c9d77b58e71b1d56f3785e2
Rambabu Chimata <Rambabu.Chimata@wnco.com>
2016-10-05 14:25:24 +0530

    Create order request formation

--- routes/wineDirect.js
+++ routes/wineDirect.js
@@ -27,8 +27,6 @@ var products = router.get('/getProducts/:id', function(req, res, next) {
 
 var order = router.post('/createOrder', function(req, res, next) {
     log.debug('order data from shopify', req.body);
-    console.log('order data from shopify', req.body)
-
     var createOrderRequest = {
         SubmitOrdersRequest: {
             AuthenticationInfo: {
@@ -36,13 +34,54 @@ var order = router.post('/createOrder', function(req, res, next) {
                 Password: 'WineDirect'
             },
             AccountNumber: '657076',
-            Orders: {
+            Orders: [{
                 DtcOrder: {
-OrderId:req.data.id
-                }
-            }
+                    OrderId: req.body.id,
+                    SoldTo: {
+                        ConsumerId: req.body.customer.id,
+                        FirstName: req.body.customer.first_name,
+                        LastName: req.body.customer.last_name,
+                        EmailAddress: req.body.customer.email,
+                        TelephoneNumber: req.body.customer.default_address.phone,
+                        Address: {
+                            CompanyName: req.body.customer.default_address.company,
+                            Line1: req.body.customer.default_address.address1,
+                            Line2: req.body.customer.default_address.address2,
+                            City: req.body.customer.default_address.city,
+                            StateOrProvince: req.body.customer.default_address.province,
+                            PostalCode: req.body.customer.default_address.zip,
+                            CountryCode: req.body.customer.default_address.country_code
+                        }
+                    },
+                    ShipTo: {
+                        FirstName: req.body.shipping_address.first_name,
+                        LastName: req.body.shipping_address.last_name,
+                        TelephoneNumber: req.body.shipping_address.phone,
+                        Address: {
+                            CompanyName: req.body.shipping_address.company,
+                            Line1: req.body.shipping_address.address1,
+                            Line2: req.body.shipping_address.address2,
+                            City: req.body.shipping_address.city,
+                            StateOrProvince: req.body.shipping_address.province,
+                            PostalCode: req.body.shipping_address.zip,
+                            CountryCode: req.body.shipping_address.country_code
+                        }
+                    },
+                    TotalSale: req.body.total_price,
+                    OrderLines: [{
+                        DtcOrderLine: {
+                            LineNumber: req.body.line_items[0].id,
+                            Quantity: req.body.line_items[0].quantity,
+                            SKU: req.body.line_items[0].sku,
+                            UnitPrice: req.body.line_items[0].price
+                        }
+                    }],
+                },
+
+            }]
         }
     }
+    console.log('create order request ', createOrderRequest);
     res.status(200).send(req.body);
 });
 
